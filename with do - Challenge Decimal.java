0a1b58bf36e324846ecc3019d9a546d8a8ff6ac0
Neo Jackal <113769888+neo-jackal@users.noreply.github.com>
2022-11-06 19:48:49 +0700

    Create with do - Challenge Decimal.java

---
+++ with do - Challenge Decimal.java
@@ -0,0 +1,39 @@
+package challenge;
+import java.util.Scanner;
+
+public class Challenge {
+
+    public static void main(String[] args) {
+// KALO MAU COPY DARI SINI YA ADIK ADIK==================================================
+        
+//                Deklarasi
+        Scanner input = new Scanner(System.in);
+        int  nilai;
+        float nilay, mean;
+                
+//                proses
+        System.out.print ("berapa kali ingin menginput nilai ? : ");
+        nilai = input.nextInt();
+        float all=0;
+        int nomor=1;
+        
+         do{    
+            System.out.print("masukan nilai ke " + nomor + ": ");
+            nilay = input.nextInt();
+            all += nilay;
+            nomor++;    
+           }while(nomor<=nilai);   
+     
+        mean = all / nilai;
+
+        if (mean <= 75) {
+            String hasil = "Tidak lulus";
+            System.out.println("kami putuskan bahwa antum " + hasil + " dengan nilai rata rata " + mean);
+        } else if (mean >= 76 || mean <= 99) {
+            String hasil = "lulus";
+            System.out.println("kami putuskan bahwa antum " + hasil + " dengan nilai rata rata " + mean);
+
+        }
+//     SAMPAI SINI ==============================================
+    }
+}
